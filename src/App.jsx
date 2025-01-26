66e4406257f428921ec6e90462ad423acf33edfd
LucioPalmucci <lucio.palmucci@hotmail.com>
2025-01-26 14:47:54 -0300

    Fist commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { fa3 } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './App.css'
+
+function App() {
+
+    return (
+        <div className=''>
+            <p className="text-3xl font-Raleway">hola mundo</p>
+            <FontAwesomeIcon icon={fa3} />
+            <p className="text-3xl">hola mundo</p>
+        </div>
+
+    )
+}
+
+export default App
