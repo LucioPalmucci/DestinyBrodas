ad5808c680a74eac29afc1eab56184e5b13fa7c9
LucioPalmucci <lucio.palmucci@hotmail.com>
2025-03-06 14:28:09 -0300

    Pve & PvP weapons + small spinner

--- src/Components/MemberPage/FavouriteWeapons.jsx
+++ src/Components/MemberPage/FavouriteWeapons.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import crownIcon from "../../assets/crown-solid.svg";
 import Spinner from "../Spinner";
@@ -6,8 +7,9 @@ import Spinner from "../Spinner";
 const API_KEY = "f83a251bf2274914ab739f4781b5e710";
 
 const DestinyTopWeapons = ({ userId, membershipType }) => {
-    const [weapons, setWeapons] = useState([]);
+    const [weapons, setWeapons] = useState({ pve: [], pvp: [] });
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState("pve");
 
     useEffect(() => {
         const fetchTopWeapons = async () => {
@@ -20,13 +22,13 @@ const DestinyTopWeapons = ({ userId, membershipType }) => {
                 );
                 const characterIds = profileRes.data.Response.profile.data.characterIds;
 
-                let weaponKills = {};
-                let precisionKills = {};
-                let precisionPro = {};
+                let weaponKills = { pve: {}, pvp: {} };
+                let precisionKills = { pve: {}, pvp: {} };
+                let precisionPro = { pve: {}, pvp: {} };
 
                 for (const characterId of characterIds) {
                     const activitiesRes = await axios.get(
-                        `/api/Platform/Destiny2/${membershipType}/Account/${userId}/Character/${characterId}/Stats/Activities/?count=10`,
+                        `/api/Platform/Destiny2/${membershipType}/Account/${userId}/Character/${characterId}/Stats/Activities/?count=5`,
                         { headers: { "X-API-Key": API_KEY } }
                     );
                     const activities = activitiesRes.data.Response?.activities || [];
@@ -43,37 +45,43 @@ const DestinyTopWeapons = ({ userId, membershipType }) => {
                         );
 
                         if (playerEntry?.extended?.weapons) {
-                            console.log(playerEntry.extended.weapons);
+                            const mode = activity.activityDetails.modes.includes(5) ? "pvp" : "pve";
                             for (const weapon of playerEntry.extended.weapons) {
                                 const weaponId = weapon.referenceId;
-                                weaponKills[weaponId] = (weaponKills[weaponId] || 0) + weapon.values.uniqueWeaponKills.basic.value;
-                                precisionKills[weaponId] = (precisionKills[weaponId] || 0) + weapon.values.uniqueWeaponPrecisionKills.basic.value;
-                                precisionPro[weaponId] = (precisionKills[weaponId] / weaponKills[weaponId]) || 0;
+                                weaponKills[mode][weaponId] = (weaponKills[mode][weaponId] || 0) + weapon.values.uniqueWeaponKills.basic.value;
+                                precisionKills[mode][weaponId] = (precisionKills[mode][weaponId] || 0) + weapon.values.uniqueWeaponPrecisionKills.basic.value;
+                                precisionPro[mode][weaponId] = (precisionKills[mode][weaponId] / weaponKills[mode][weaponId]) || 0;
                             }
                         }
                     }
                 }
 
-                let weaponDetails = await Promise.all(
-                    Object.keys(weaponKills).map(async (weaponId) => {
-                        const itemRes = await axios.get(
-                            `/api/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/${weaponId}/?lc=es`,
-                            { headers: { "X-API-Key": API_KEY } }
-                        );
-                        const itemData = itemRes.data.Response;
-                        console.log(itemData);
-                        return {
-                            name: itemData.displayProperties.name,
-                            icon: `/api${itemData.displayProperties.icon}`,
-                            kills: weaponKills[weaponId],
-                            precisionKills: precisionKills[weaponId],
-                            precisionPro: (precisionPro[weaponId] * 100).toFixed(0),
-                        };
-                    })
-                );
+                const getWeaponDetails = async (mode) => {
+                    return await Promise.all(
+                        Object.keys(weaponKills[mode]).map(async (weaponId) => {
+                            const itemRes = await axios.get(
+                                `/api/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/${weaponId}/?lc=es`,
+                                { headers: { "X-API-Key": API_KEY } }
+                            );
+                            const itemData = itemRes.data.Response;
+                            return {
+                                name: itemData.displayProperties.name,
+                                icon: `/api${itemData.displayProperties.icon}`,
+                                kills: weaponKills[mode][weaponId],
+                                precisionKills: precisionKills[mode][weaponId],
+                                precisionPro: (precisionPro[mode][weaponId] * 100).toFixed(0),
+                            };
+                        })
+                    );
+                };
 
-                weaponDetails = weaponDetails.sort((a, b) => b.kills - a.kills).slice(0, 5);
-                setWeapons(weaponDetails);
+                const pveWeapons = await getWeaponDetails("pve");
+                const pvpWeapons = await getWeaponDetails("pvp");
+
+                setWeapons({
+                    pve: pveWeapons.sort((a, b) => b.kills - a.kills).slice(0, 5),
+                    pvp: pvpWeapons.sort((a, b) => b.kills - a.kills).slice(0, 5),
+                });
             } catch (error) {
                 console.error("Error fetching weapons:", error);
             } finally {
@@ -85,62 +93,73 @@ const DestinyTopWeapons = ({ userId, membershipType }) => {
     }, [userId, membershipType]);
 
     return (
-        <div className="p-4 bg-neutral-800 text-white rounded-lg shadow-md w-fit">
-            <h2 className="text-xl font-bold">Armas mas usadas</h2>
+        <div className="p-4 bg-neutral-300 rounded-lg shadow-md w-2xs">
+            <h2 className="text-xl font-bold">Armas más usadas</h2>
             <p className="text-xs italic">Últimos 7 días</p>
+            <div className="flex justify-center mb-4 mt-1">
+                <button onClick={() => setPage("pve")} className={`cursor-pointer text-sm p-2 py-1 ${page === "pve" ? "bg-gray-400" : ""} rounded-lg`}>PvE</button>
+                <button onClick={() => setPage("pvp")} className={`cursor-pointer text-sm p-2 py-1 ${page === "pvp" ? "bg-gray-400" : ""} rounded-lg`}>PvP</button>
+            </div>
             {loading ? (
-                <Spinner />
+                <Spinner small={true} />
             ) : (
-                <ul className="space-y-3 mt-4">
-                    {weapons.map((weapon, index) => (
-                        console.log(weapon),
-                        <li
-                            key={index}
-                            className={`flex text-start items-center`}
-                        >
-                            <div className="flex items-center">
-                                <div className={`relative`}>
-                                    <img
-                                        src={weapon.icon}
-                                        width={40}
-                                        height={40}
-                                        alt="Weapon Icon"
-                                        className='rounded'
-                                        style={
-                                            index === 0
-                                                ? { filter: "drop-shadow(0 0 5px #f6e05e)" }
-                                                : index === 1
-                                                ? { filter: "drop-shadow(0 0 5px #c0c0c0)" }
-                                                : index === 2
-                                                ? { filter: "drop-shadow(0 0 5px #8b4513)" }
-                                                : {}
-                                        }
-                                    />
-                                    {index === 0 && (
+                <AnimatePresence mode="wait">
+                    <motion.ul
+                        key={page}
+                        initial={{ opacity: 0, x: page === "pve" ? -10 : 10 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: 0 }}
+                        className="space-y-3 mt-4"
+                    >
+                        {weapons[page].map((weapon, index) => (
+                            <li
+                                key={index}
+                                className={`flex text-start`}
+                            >
+                                <div className="flex items-center">
+                                    <div className="relative flex-shrink-0">
                                         <img
-                                            src={crownIcon}
-                                            alt="Crown Icon"
-                                            className="absolute -top-0 right-0 transform translate-x-1/2 -translate-y-1/2 w-6 h-6 rotate-35"
-                                            style={{ filter: "invert(70%) sepia(90%) saturate(500%) hue-rotate(-10deg) brightness(120%)" }}
+                                            src={weapon.icon}
+                                            width={40}
+                                            height={40}
+                                            alt="Weapon Icon"
+                                            className='rounded'
+                                            style={
+                                                index === 0
+                                                    ? { filter: "drop-shadow(0 0 5px #f6e05e)" }
+                                                    : index === 1
+                                                        ? { filter: "drop-shadow(0 0 5px rgb(119, 119, 119))" }
+                                                        : index === 2
+                                                            ? { filter: "drop-shadow(0 0 5px #8b4513)" }
+                                                            : {}
+                                            }
                                         />
-                                    )}
-                                </div>
-                                <div className="ml-4 items-center">
-                                    <div>
-                                        <p className={`text-sm`}>{weapon.name}</p>
-                                        <p className={`text-xs flex justify-start`}>
-                                            <p className={`mr-1`}><i className="icon-kills" />{weapon.kills}</p>
-                                            <p title={`${weapon.precisionKills} bajas`}>
+                                        {index === 0 && (
+                                            <img
+                                                src={crownIcon}
+                                                alt="Crown Icon"
+                                                className="absolute -top-0 right-0 transform translate-x-1/2 -translate-y-1/2 w-6 h-6 rotate-35"
+                                                style={{ filter: "invert(70%) sepia(90%) saturate(500%) hue-rotate(-10deg) brightness(120%)" }}
+                                            />
+                                        )}
+                                    </div>
+                                    <div className="ml-4 items-center">
+                                        <p className="text-sm">{weapon.name}</p>
+                                        <p className="text-xs flex space-x-2">
+                                            <p>
+                                                <i className="icon-kills-black" />{weapon.kills}
+                                            </p>
+                                            <p title={`${weapon.precisionKills} bajas`} className="flex">
                                                 <i className="icon-precision" style={{ fontStyle: "normal" }} />
                                                 {weapon.precisionPro}%
                                             </p>
                                         </p>
                                     </div>
                                 </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+                            </li>
+                        ))}
+                    </motion.ul>
+                </AnimatePresence>
             )}
         </div>
     );
--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Spinner.css';
 
-export default function Spinner() {
-    return (
+export default function Spinner({ small }) {
+    if (small) {
+        return (
+            <div className="spinner-small">
+                <div className="small-spinner-wheel"></div>
+            </div>
+        );
+    }
+    else return (
         <div className="spinner">
             <div className="spinner-wheel"></div>
         </div>
